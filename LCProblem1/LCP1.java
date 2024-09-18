1e908364ddc51aa8caf52ef7e4b9caa7cae4c958
Zacharyrg25 <zacharyrg25@gmail.com>
2024-09-18 16:56:59 -0700

    Committed

--- LCProblem1/LCP1.java
+++ LCProblem1/LCP1.java
@@ -2,10 +2,12 @@ package LCProblem1; //Do not change the packatge name
 
 public class LCP1 { //Do not change the class name
     public static void main(String[] args) { 
-        
+        double myDouble = 5.5;
+        int myInt = 10;
+        String myString = "Hello World";
 
-        //Your code goes here
-
-        
+        System.out.println(myDouble);
+        System.out.println(myInt);
+        System.out.println(myString); 
     }
 }
