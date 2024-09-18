c75b5b6def0afd53f6570a1e16e67787f63cbe54
cezuhr <cballesteros2@students.avc.edu>
2024-09-18 16:51:03 -0700

    Finished Java portion

--- LCProblem1/LCP1.java
+++ LCProblem1/LCP1.java
@@ -3,9 +3,20 @@ package LCProblem1; //Do not change the packatge name
 public class LCP1 { //Do not change the class name
     public static void main(String[] args) { 
         
-
+    
         //Your code goes here
-
-        
+    
+    
+    
+    //Prints each of these variables to the console, each on a new line.
+        double num1 = 5.5;
+        // Initializes a double variable with a value of 5.5.
+        int num2 = 10;
+        //Initializes an integer variable with a value of 10.
+        String helloWorld = "Hello World";
+        //Initializes a string variable with a value of "Hello World".
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(helloWorld);
     }
 }
