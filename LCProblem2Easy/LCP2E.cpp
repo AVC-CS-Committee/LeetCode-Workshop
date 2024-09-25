162f9e2a5f9d14377fa72bf3b116c281d8d5f677
Ryan Reeves <ryanreevesyt@gmail.com>
2024-09-24 17:08:43 -0700

    questions from previous commit added

---
+++ LCProblem2Easy/LCP2E.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  // This is the standard header file for input/output in C++.
+using namespace std; // This line is used to include the standard namespace in the program.
+
+int main()
+{
+
+    // Your code goes here
+
+    
+
+    return 0; // Ignore this line (do not delete it)
+}
---
+++ LCProblem2Easy/LCP2E.py
@@ -0,0 +1,9 @@
+def LCProblem2Easy(): #this function prints the variables (do not remove this function)
+    
+
+
+    #Your code goes here
+    return 0 #delete this line when you start coding
+
+    
+LCProblem2Easy() #this line calls the function to print the variables (do not remove this line)
