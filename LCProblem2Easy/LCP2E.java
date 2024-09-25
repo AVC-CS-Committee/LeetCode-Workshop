60eb4dbcc2c93e62391a58bec28f0de180fdd1f2
Blake Mosley <33817212+busterblake@users.noreply.github.com>
2024-09-25 23:39:24 +0000

    done

--- LCProblem2Easy/LCP2E.java
+++ LCProblem2Easy/LCP2E.java
@@ -2,10 +2,14 @@ package LCProblem2Easy; //Do not change the package name
 
 public class LCP2E { // Do not change the class name
     public static void main(String[] args) {
-
-        // Your code goes here
-
+        int sum = 0;
+        int[] arr = {100, 200, 300, 400};
         
-
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > 0){
+                sum += arr[i];
+            };
+        };
+        System.out.println(sum);
     }
 }
