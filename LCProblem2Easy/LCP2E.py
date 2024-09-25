2773737c2263bfbe5fa246d940339f67ef1ecca6
paoloprogram12 <sethpsalazar@gmail.com>
2024-09-25 16:56:07 -0700

    Finished

--- LCProblem2Easy/LCP2E.py
+++ LCProblem2Easy/LCP2E.py
@@ -1,11 +1,15 @@
-# def LCProblem2Easy(): #this function prints the variables (do not remove this function)
+def LCProblem2Easy(): #this function prints the variables (do not remove this function)
+    listLength = input('Enter the length of list: ')
+    numList = []
+    for i in range(int(listLength)):
+        num = int(input('Enter a number: '))
+        numList.append(num)
+
+    addSum = 0
+    for i in numList:
+        addSum += i
     
-
-
-    #Your code goes here
-    # return 0 #delete this line when you start coding
+    return addSum
 
     
-# print(LCProblem2Easy()) #this line calls the function to print the variables (do not remove this line)
-
-print("hello world")
+print(LCProblem2Easy()) #this line calls the function to print the variables (do not remove this line)
