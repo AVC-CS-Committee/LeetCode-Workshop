10c41ebc9ecd470e5e0fdc75510a84271dd02b21
Ryan Reeves <ryanreevesyt@gmail.com>
2024-10-02 16:22:13 -0700

    My sol to LCP3H

--- LCProblem3Hard/LCP3H.py
+++ LCProblem3Hard/LCP3H.py
@@ -1,9 +1,27 @@
+from collections import deque
+
+s = "([{}])" #this is the example string
+
 def LCProblem2Hard(): #this function prints the variables (do not remove this function)
     
-
-
-    #Your code goes here
-    return 0 #delete this line when you start coding
+    stack = deque()
+            
+    for i in s:
+        if i == '(' or i == '[' or i == '{': #checks to see if the character is an opening bracket
+            stack.append(i)
+        else:
+            if not stack: #instantly returns false if the stack is empty
+                return False
+            #these if statemetns check to see if the closing bracket matches the opening bracket
+            if i == ')' and stack[-1] == '(':
+                stack.pop()
+            elif i == ']' and stack[-1] == '[':
+                stack.pop()
+            elif i == '}' and stack[-1] == '{':
+                stack.pop()
+            else:
+                return False
+    return True
 
     
 print(LCProblem2Hard()) #this line calls the function to print the variables (do not remove this line)
