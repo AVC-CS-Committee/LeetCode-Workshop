b1c1226e895753d5c5d4afc8fece258fb086540d
Ryan Reeves <ryanreevesyt@gmail.com>
2024-10-02 14:02:20 -0700

    10/2 Half Updated

---
+++ LCProblem3Hard/LCP3H.java
@@ -0,0 +1,9 @@
+package LCProblem3Hard; //Do not change the package name
+
+public class LCP3H { // Do not change the class name
+    public static void main(String[] args) {
+
+        // Your code goes here
+
+    }
+}
