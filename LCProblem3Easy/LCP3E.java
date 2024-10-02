d83fb8f7d86511e847dc2d4e74f6cc68a218d63d
paoloprogram12 <sethpsalazar@gmail.com>
2024-10-02 16:39:02 -0700

    Update LCP3E.java

--- LCProblem3Easy/LCP3E.java
+++ LCProblem3Easy/LCP3E.java
@@ -1,11 +1,28 @@
 package LCProblem3Easy; //Do not change the package name
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class LCP3E { // Do not change the class name
+
+    static int listNum;
+    static List<Integer> listNumbers = new ArrayList<>();
+
+    public static void createList() {
+        System.out.print("Enter count in list: ");
+        Scanner scnr = new Scanner(System.in);
+        listNum = scnr.nextInt();
+        System.out.println();
+        for (int i = 0; i < listNum; i++) {
+            System.out.println("Enter a " + i + "st" + "number");
+            listNumbers.add(scnr.nextInt());
+        }
+    }
+
     public static void main(String[] args) {
 
-        // Your code goes here
-
-        
+        createList(); 
 
     }
 }
