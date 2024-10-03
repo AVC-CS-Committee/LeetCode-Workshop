ea1827740fd2322c94e3eeb9b1580d646c342e09
paoloprogram12 <sethpsalazar@gmail.com>
2024-10-02 17:14:30 -0700

    Finished Easy problem

--- LCProblem3Easy/LCP3E.py
+++ LCProblem3Easy/LCP3E.py
@@ -1,9 +1,15 @@
 def LCProblem2Easy(): #this function prints the variables (do not remove this function)
-    
+    listLength = input("Enter list number: ")
+    numList = []
+    for i in range(int(listLength)):
+        num = int(input("Enter a number: "))
+        numList.append(num)
 
-
-    #Your code goes here
-    return 0 #delete this line when you start coding
+    evenCount = 0
+    for i in numList:
+        if i % 2 == 0:
+            evenCount += 1
+    print(evenCount)
 
     
 print(LCProblem2Easy()) #this line calls the function to print the variables (do not remove this line)
